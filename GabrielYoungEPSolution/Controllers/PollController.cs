8178c58d3fac0bd455b2bb65dfb9879d3a49258b
Gabriel Young <gabriel.young.g52202@mcast.edu.mt>
2025-04-04 00:22:30 +0200

    Created 'Create' methods in PollController

--- GabrielYoungEPSolution/Controllers/PollController.cs
+++ GabrielYoungEPSolution/Controllers/PollController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
     public class PollController : Controller
     {
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create([FromServices] PollRepository pollRepository)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromServices] PollRepository pollRepository, string title, string option1Text, string option2Text, string option3Text)
+        {
+            var poll = new Domain.Models.Poll
+            {
+                Title = title,
+                Option1Text = option1Text,
+                Option2Text = option2Text,
+                Option3Text = option3Text,
+                Option1VotesCount = 0,
+                Option2VotesCount = 0,
+                Option3VotesCount = 0,
+                DateCreated = DateTime.Now
+            };
+
+            pollRepository.CreatePoll(poll);
+            return RedirectToAction("Index");
         }
     }
 }
