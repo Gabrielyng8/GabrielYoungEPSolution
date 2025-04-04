a792f79389acaaa2dd5457d5c167be51663bbed9
Gabriel Young <gabriel.young.g52202@mcast.edu.mt>
2025-04-04 14:02:41 +0200

    Add Newtonsoft.Json and implement PollFileRepository
    Added Newtonsoft.Json package version 13.0.3 to DataAccess.csproj, Domain.csproj, and Presentation.csproj for JSON serialization and deserialization.
    
    Created PollFileRepository class in DataAccess.Repositories namespace to handle creation and retrieval of Poll objects from a file using JSON. Implemented CreatePoll and GetPolls methods for managing poll data.

---
+++ DataAccess/Repositories/PollFileRepository.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace DataAccess.Repositories
+{
+    public class PollFileRepository
+    {
+        private readonly string _filePath;
+
+        public PollFileRepository(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void CreatePoll(Poll poll)
+        {
+            var polls = GetPolls().ToList();
+            poll.Id = polls.Any() ? polls.Max(p => p.Id) + 1 : 1;
+            polls.Add(poll);
+            File.WriteAllText(_filePath, JsonConvert.SerializeObject(polls, Newtonsoft.Json.Formatting.Indented));
+        }
+
+        public IEnumerable<Poll> GetPolls()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Poll>();
+            }
+
+            var jsonData = File.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<List<Poll>>(jsonData) ?? new List<Poll>();
+        }
+    }
+}
